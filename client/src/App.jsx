42a3dbe281b4183cbf7300ed3d4cd1a538dacd53
Raktim Banerjee <raktimbanerjee05@gmail.com>
2024-01-26 15:51:06 +0530

    install react js and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function App() {
+  return (
+    <div className="h-[100%] w-[100%] bg-slate-600">
+      <h1 className='text-slate-300'>App</h1>
+    </div>
+  )
+}
