498aebac644986128fe7d92f48c922b6c554068a
Raktim Banerjee <raktimbanerjee05@gmail.com>
2024-01-27 15:32:31 +0530

    create user model

---
+++ api/models/user.js
@@ -0,0 +1,21 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+    
+}, { timestamps: true });
+
+model.exports= mongoose.model('User', userSchema);
